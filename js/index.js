8389d7de11adf5a093159e0311e1b6b5f537618f
haowanwen <1225216451@qq.com>
2019-08-29 23:43:36 +0800

    ReadManagement第一次提交

---
+++ js/index.js
@@ -0,0 +1,29 @@
+// 主js
+// 点击左侧导航,右侧出现对应内容
+$(function(){
+	// $('.info-content').load('./pages/home.html');
+	$('.info-content').load('./pages/home.html');
+	// $('.info-content').load('./pages/info.html');
+	// $('.info-content').load('./pages/user.html');
+	$('.info-left li').click(function(){
+		// this-->li DOM
+		var title = $(this).text().trim();
+		switch (title) {
+			case '首页':
+				$('.info-content').load('./pages/home.html');
+				break;
+			case '栏目管理':
+				$('.info-content').load('./pages/category.html');
+				break;
+			case '咨询管理':
+				$('.info-content').load('./pages/info.html');
+				break;
+			case '用户管理':
+				$('.info-content').load('./pages/user.html');
+				break;
+			default:
+				// statements_def
+				break;
+		}
+	});
+});
